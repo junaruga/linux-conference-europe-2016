c4173f6eb9cd825bf2957d3f1479c911ab467058
Jun Aruga <jaruga@redhat.com>
2016-10-03 09:26:39 +0200

    Add documents. - Community to Enterprise - Classic IT and Cloud IT

---
+++ slide/rh_upstream_slide.mm
@@ -0,0 +1,42 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1475433017222" ID="ID_605971343" MODIFIED="1475433089061" TEXT="COMMUNITY TO ENTERPRISE">
+<node CREATED="1475433093037" ID="ID_10764343" LINK="http://www.projectatomic.io/" MODIFIED="1475433942682" POSITION="right" TEXT="Project ATOMIC">
+<node CREATED="1475433103274" ID="ID_1495586181" LINK="https://www.redhat.com/en/resources/red-hat-enterprise-linux-atomic-host" MODIFIED="1475434121058" TEXT="REDHAT ENTERPRISE LINUX ATOMIC HOST"/>
+</node>
+<node CREATED="1475433129312" ID="ID_1617261511" LINK="http://manageiq.org/" MODIFIED="1475434346506" POSITION="right" TEXT="ManageIQ">
+<node CREATED="1475433137170" ID="ID_1074115531" LINK="https://www.redhat.com/en/technologies/management/cloudforms" MODIFIED="1475434384634" TEXT="RED HAT CLOUDFORMS"/>
+</node>
+<node CREATED="1475433146476" ID="ID_276388199" LINK="http://www.jboss.org/" MODIFIED="1475434517682" POSITION="right" TEXT="JBoss Community FuseSource">
+<node CREATED="1475433155934" ID="ID_1406437950" LINK="https://www.redhat.com/en/technologies/jboss-middleware" MODIFIED="1475434584930" TEXT="RED HAT JBOSS MIDDLEWARE"/>
+</node>
+<node CREATED="1475433166159" ID="ID_1051724983" LINK="https://www.openshift.org/" MODIFIED="1475434662204" POSITION="right" TEXT="OPENSHIFT origin">
+<node CREATED="1475433174058" ID="ID_1972429904" LINK="https://www.openshift.com/" MODIFIED="1475434692729" TEXT="OPENSHIFT by RED HAT"/>
+</node>
+<node CREATED="1475433182981" ID="ID_1163585315" LINK="http://feedhenry.org/" MODIFIED="1475438338667" POSITION="right" TEXT="FEEDHENRY">
+<node CREATED="1475433186170" ID="ID_1109052499" LINK="https://www.redhat.com/en/technologies/mobile" MODIFIED="1475434830793" TEXT="RED HAT MOBILE"/>
+</node>
+<node CREATED="1475433235322" ID="ID_1134922637" MODIFIED="1475433247094" POSITION="left" TEXT="Other Communities">
+<node CREATED="1475433195933" ID="ID_990041246" LINK="https://www.apache.org/" MODIFIED="1475436668611" TEXT="Apache Software Foundation"/>
+<node CREATED="1475433213220" ID="ID_585213885" LINK="http://www.patternfly.org/" MODIFIED="1475438433523" TEXT="PATTERNFLY"/>
+<node CREATED="1475433258410" ID="ID_1108613775" LINK="https://www.centos.org/" MODIFIED="1475436690765" TEXT="CentOS"/>
+<node CREATED="1475433262935" ID="ID_761693416" LINK="https://www.kernel.org/" MODIFIED="1475436731074" TEXT="Linux Kernel"/>
+<node CREATED="1475433266874" ID="ID_507300265" LINK="http://www.openstack.org/" MODIFIED="1475436773489" TEXT="OpenStack"/>
+</node>
+<node CREATED="1475433289676" ID="ID_1438700832" LINK="http://www.ovirt.org/" MODIFIED="1475433898579" POSITION="left" TEXT="oVirt">
+<node CREATED="1475433296818" ID="ID_1745497593" LINK="https://access.redhat.com/products/red-hat-virtualization" MODIFIED="1475433836987" TEXT="RED HAT ENTERPRISE VIRTUALIZATION"/>
+</node>
+<node CREATED="1475433313815" ID="ID_234650681" LINK="http://ceph.com/" MODIFIED="1475437405797" POSITION="left" TEXT="Gluster, ceph">
+<node CREATED="1475433319945" ID="ID_1272788883" LINK="https://www.redhat.com/en/technologies/storage" MODIFIED="1475434882513" TEXT="RED HAT STORAGE"/>
+</node>
+<node CREATED="1475433327080" ID="ID_1655832411" LINK="https://theforeman.org/" MODIFIED="1475436345375" POSITION="left" TEXT="FOREMAN">
+<node CREATED="1475433330949" ID="ID_1062044621" LINK="https://www.redhat.com/en/technologies/management/satellite" MODIFIED="1475436379167" TEXT="RED HAT SATELLITE"/>
+</node>
+<node CREATED="1475433342744" ID="ID_1681718316" LINK="https://www.rdoproject.org/" MODIFIED="1475436487700" POSITION="left" TEXT="RDO">
+<node CREATED="1475433346776" ID="ID_1557664223" LINK="https://www.redhat.com/en/technologies/linux-platforms/openstack-platform" MODIFIED="1475436419559" TEXT="RED HAT ENTERPRISE OPENSTACK PLATFORM"/>
+</node>
+<node CREATED="1475433069986" ID="ID_104182939" LINK="https://getfedora.org/en/" MODIFIED="1475436600912" POSITION="left" TEXT="Fedora">
+<node CREATED="1475433365188" ID="ID_167498366" LINK="https://www.redhat.com/en/technologies/linux-platforms/enterprise-linux" MODIFIED="1475436550523" TEXT="RED HAT ENTERPRISE LINUX"/>
+</node>
+</node>
+</map>
